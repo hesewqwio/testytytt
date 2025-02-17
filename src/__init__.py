0b76e8ea749d9644c787292157b12147b6e5147e
Tedyh <128471650+hesewqwio@users.noreply.github.com>
2025-02-17 11:39:29 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .browser import Browser
+from .searches import Searches
+from .userAgentGenerator import GenerateUserAgent
+from .utils import CONFIG, getProjectRoot, getBrowserConfig, saveBrowserConfig
