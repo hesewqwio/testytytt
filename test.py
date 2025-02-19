7305957f89fae05831d1f13ace3fefa6f2b3f771
Tedyh <128471650+hesewqwio@users.noreply.github.com>
2025-02-19 09:34:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,258 +1,223 @@
-import contextlib
-import locale
+import dbm.dumb
+import json
 import logging
-import os
-import random
-from pathlib import Path
+import shelve
+from datetime import date, timedelta
+from enum import Enum, auto
+from itertools import cycle
+from random import random, randint, shuffle
 from time import sleep
-from typing import Any, Type, Union
-from urllib.parse import urlparse
-from types import TracebackType
+from typing import Final
 
-import ipapi
-import pycountry
-import seleniumwire.undetected_chromedriver as webdriver
-import undetected_chromedriver
-from ipapi.exceptions import RateLimited
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.chrome.webdriver import WebDriver
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
+import contextlib
 
-from src.utils import CONFIG, getBrowserConfig, getProjectRoot, saveBrowserConfig
-from src.userAgentGenerator import GenerateUserAgent
+from src.browser import Browser
+from src.utils import CONFIG, makeRequestsSession, getProjectRoot
 
-class Browser:
-    """WebDriver wrapper class."""
 
-    webdriver: Union[undetected_chromedriver.Chrome, None]
+class RetriesStrategy(Enum):
+    """
+    method to use when retrying
+    """
 
-    def __init__(self, mobile: bool) -> None:
-        logging.debug("in __init__")
-        self.mobile = mobile
-        self.browserType = "mobile" if mobile else "desktop"
-        self.headless = not CONFIG.browser.visible
-        self.localeLang, self.localeGeo = self.getLanguageCountry()
-        self.proxy = CONFIG.browser.proxy  # Proxy setting from config
-        self.userDataDir = self.setupProfiles()
-        self.browserConfig = getBrowserConfig(self.userDataDir)
-        (
-            self.userAgent,
-            self.userAgentMetadata,
-            newBrowserConfig,
-        ) = GenerateUserAgent().userAgent(self.browserConfig, mobile)
-        if newBrowserConfig:
-            self.browserConfig = newBrowserConfig
-            saveBrowserConfig(self.userDataDir, self.browserConfig)
-        self.webdriver = self.browserSetup()
-        logging.debug("out __init__")
+    EXPONENTIAL = auto()
+    """
+    an exponentially increasing `base_delay_in_seconds` between attempts
+    """
+    CONSTANT = auto()
+    """
+    the default; a constant `base_delay_in_seconds` between attempts
+    """
+
+
+class Searches:
+    maxRetries: Final[int] = CONFIG.get("retries").get("max")
+    """
+    the max amount of retries to attempt
+    """
+    baseDelay: Final[float] = CONFIG.get("retries").get("base_delay_in_seconds")
+    """
+    how many seconds to delay
+    """
+    # retriesStrategy = Final[  # todo Figure why doesn't work with equality below
+    retriesStrategy = RetriesStrategy[CONFIG.get("retries").get("strategy")]
+
+    def __init__(self, browser: Browser):
+        self.browser = browser
+        self.webdriver = browser.webdriver
+
+        dumbDbm = dbm.dumb.open((getProjectRoot() / "google_trends").__str__())
+        self.googleTrendsShelf: shelve.Shelf = shelve.Shelf(dumbDbm)
 
     def __enter__(self):
-        logging.debug("in __enter__")
         return self
 
-    def __exit__(
-        self,
-        exc_type: Type[BaseException] | None,
-        exc_value: BaseException | None,
-        traceback: TracebackType | None,
-    ):
-        logging.debug(
-            f"in __exit__ exc_type={exc_type} exc_value={exc_value} traceback={traceback}"
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.googleTrendsShelf.__exit__(None, None, None)
+
+    def getGoogleTrends(self, wordsCount: int) -> list[str]:
+        # Function to retrieve Google Trends search terms
+        searchTerms: list[str] = []
+        i = 0
+        session = makeRequestsSession()
+        while len(searchTerms) < wordsCount:
+            i += 1
+            # Fetching daily trends from Google Trends API
+            r = session.get(
+                f"https://trends.google.com/trends/api/dailytrends?hl={self.browser.localeLang}"
+                f'&ed={(date.today() - timedelta(days=i)).strftime("%Y%m%d")}&geo={self.browser.localeGeo}&ns=15'
+            )
+            assert (
+                r.status_code == requests.codes.ok
+            ), "Adjust retry config in src.utils.Utils.makeRequestsSession"
+            trends = json.loads(r.text[6:])
+            for topic in trends["default"]["trendingSearchesDays"][0][
+                "trendingSearches"
+            ]:
+                searchTerms.append(topic["title"]["query"].lower())
+                searchTerms.extend(
+                    relatedTopic["query"].lower()
+                    for relatedTopic in topic["relatedQueries"]
+                )
+            searchTerms = list(set(searchTerms))
+        del searchTerms[wordsCount : (len(searchTerms) + 1)]
+        return searchTerms
+
+    def getRelatedTerms(self, term: str) -> list[str]:
+        # Function to retrieve related terms from Bing API
+        relatedTerms: list[str] = (
+            makeRequestsSession()
+            .get(
+                f"https://api.bing.com/osjson.aspx?query={term}",
+                headers={"User-agent": self.browser.userAgent},
+            )
+            .json()[1]
         )
-        self.webdriver.close()
-        self.webdriver.quit()
+        uniqueTerms = list(dict.fromkeys(relatedTerms))  # Remove duplicates
+        uniqueTerms = [t for t in uniqueTerms if t.lower() != term.lower()]  # Exclude exact term
+        return uniqueTerms
 
-    def browserSetup(self) -> undetected_chromedriver.Chrome:
-        options = undetected_chromedriver.ChromeOptions()
-        options.headless = self.headless
-        options.add_argument(f"--lang={self.localeLang}")
-        options.add_argument("--log-level=3")
-        options.add_argument("--blink-settings=imagesEnabled=false")
-        options.add_argument("--ignore-certificate-errors")
-        options.add_argument("--ignore-certificate-errors-spki-list")
-        options.add_argument("--ignore-ssl-errors")
-        if os.environ.get("DOCKER"):
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--dns-prefetch-disable")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-default-apps")
-        options.add_argument("--disable-features=Translate")
-        options.add_argument("--disable-features=PrivacySandboxSettings4")
-        options.add_argument("--disable-http2")
-        options.add_argument("--disable-search-engine-choice-screen")
-        options.page_load_strategy = "eager"
+    def bingSearches(self, searchRelatedTerms: bool = False, relatedTermsCount: int = 0) -> None:
+        logging.info(f"[BING] Starting {self.browser.browserType.capitalize()} Edge Bing searches...")
 
-        seleniumwireOptions: dict[str, Any] = {"verify_ssl": False}
+        self.browser.utils.goToSearch()
 
-        if self.proxy:
-            parsed_proxy = urlparse(self.proxy)
-            if parsed_proxy.scheme in ["http", "https"]:
-                seleniumwireOptions["proxy"] = {
-                    "http": f"{parsed_proxy.scheme}://{parsed_proxy.hostname}:{parsed_proxy.port}",
-                    "https": f"{parsed_proxy.scheme}://{parsed_proxy.hostname}:{parsed_proxy.port}",
-                    "no_proxy": "localhost,127.0.0.1",
-                }
-                if parsed_proxy.username and parsed_proxy.password:
-                    seleniumwireOptions["proxy"]["auth"] = (parsed_proxy.username, parsed_proxy.password)
-            elif parsed_proxy.scheme == "socks5":
-                seleniumwireOptions["proxy"] = {
-                    "http": f"socks5://{parsed_proxy.hostname}:{parsed_proxy.port}",
-                    "https": f"socks5://{parsed_proxy.hostname}:{parsed_proxy.port}",
-                    "no_proxy": "localhost,127.0.0.1",
-                }
-                if parsed_proxy.username and parsed_proxy.password:
-                    seleniumwireOptions["proxy"]["auth"] = (parsed_proxy.username, parsed_proxy.password)
+        desktopAndMobileRemaining = self.browser.getRemainingSearches(desktopAndMobile=True)
+        remainingSearches = desktopAndMobileRemaining.getTotal()
+        searchCount = 0
+        while searchCount < remainingSearches:
+            logging.info(f"[BING] {searchCount + 1}/{remainingSearches}")
+            searchCount = self.bingSearch(searchRelatedTerms, relatedTermsCount, searchCount)
+            if searchCount >= remainingSearches:
+                break
 
-        driver = None
+            if desktopAndMobileRemaining.getTotal() > len(self.googleTrendsShelf):
+                # self.googleTrendsShelf.clear()  # Maybe needed?
+                logging.debug(
+                    f"google_trends before load = {list(self.googleTrendsShelf.items())}"
+                )
+                trends = self.getGoogleTrends(desktopAndMobileRemaining.getTotal())
+                shuffle(trends)
+                for trend in trends:
+                    self.googleTrendsShelf[trend] = None
+                logging.debug(
+                    f"google_trends after load = {list(self.googleTrendsShelf.items())}"
+                )
 
-        if os.environ.get("DOCKER"):
-            driver = webdriver.Chrome(
-                options=options,
-                seleniumwire_options=seleniumwireOptions,
-                user_data_dir=self.userDataDir.as_posix(),
-                driver_executable_path="/usr/bin/chromedriver",
-            )
+            if searchRelatedTerms:
+                rootTerm = list(self.googleTrendsShelf.keys())[0]  # Adjusted from 1 to 0
+                terms = self.getRelatedTerms(rootTerm)
+                uniqueTerms = list(dict.fromkeys(terms))
+                uniqueTerms = [t for t in uniqueTerms if t.lower() != rootTerm.lower()]
+                for i, _ in enumerate(uniqueTerms[:relatedTermsCount]):
+                    searchCount = self.bingSearch(searchRelatedTerms, relatedTermsCount, searchCount)
+                    if searchCount >= remainingSearches:
+                        break
+
+            self.bingSearch()
+            del self.googleTrendsShelf[list(self.googleTrendsShelf.keys())[0]]
+            sleep(randint(10, 15))  # Short delay added here
+
+        logging.info(f"[BING] Finished {self.browser.browserType.capitalize()} Edge Bing searches!")
+
+    def manually_enter_text(self, searchbar, term: str, min_delay: float = 0.4, max_delay: float = 0.9):
+        for char in term:
+            searchbar.send_keys(char)
+            sleep(random() * (max_delay - min_delay) + min_delay)  # Random delay between keystrokes to mimic manual typing
+
+    def bingSearch(self, searchRelatedTerms: bool = False, relatedTermsCount: int = 0, searchCount: int = 0) -> int:
+        if not self.googleTrendsShelf:
+            logging.error("googleTrendsShelf is empty, unable to proceed with search.")
+            return searchCount
+
+        pointsBefore = self.browser.utils.getAccountPoints()
+        rootTerm = list(self.googleTrendsShelf.keys())[0]  # Adjusted from 1 to 0 for consistency
+        terms = self.getRelatedTerms(rootTerm)
+        uniqueTerms = list(dict.fromkeys(terms))  # Remove duplicates
+        uniqueTerms = [t for t in uniqueTerms if t.lower() != rootTerm.lower()]  # Exclude exact root term
+        logging.debug(f"rootTerm={rootTerm}")
+        logging.debug(f"uniqueTerms={uniqueTerms}")
+
+        if searchRelatedTerms:
+            terms = [rootTerm] + uniqueTerms[:relatedTermsCount]  # Dynamically handle related terms count
         else:
-            version = self.getChromeVersion()
-            major = int(version.split(".")[0])
-            driver = webdriver.Chrome(
-                options=options,
-                seleniumwire_options=seleniumwireOptions,
-                user_data_dir=self.userDataDir.as_posix(),
-                version_main=major,
-            )
+            terms = [rootTerm]
 
-        seleniumLogger = logging.getLogger("seleniumwire")
-        seleniumLogger.setLevel(logging.ERROR)
+        logging.debug(f"terms={terms}")
 
-        if self.browserConfig.get("sizes"):
-            deviceHeight = self.browserConfig["sizes"]["height"]
-            deviceWidth = self.browserConfig["sizes"]["width"]
+        searchbar = self.browser.utils.waitUntilClickable(By.ID, "sb_form_q", timeToWait=40)
+        termsCycle: cycle[str] = cycle(terms)  # Define the terms cycle here
+        for _ in range(3):
+            searchbar.clear()
+            term = next(termsCycle)
+            logging.debug(f"term={term}")
+            self.manually_enter_text(searchbar, term, min_delay=0.4, max_delay=0.9)  # Use the manual typing function
+            with contextlib.suppress(TimeoutException):
+                WebDriverWait(self.webdriver, 40).until(
+                    expected_conditions.text_to_be_present_in_element_value((By.ID, "sb_form_q"), term)
+                )
+                break
+            logging.debug("error send_keys")
         else:
-            if self.mobile:
-                deviceHeight = random.randint(568, 1024)
-                deviceWidth = random.randint(320, min(576, int(deviceHeight * 0.7)))
-            else:
-                deviceWidth = random.randint(1024, 2560)
-                deviceHeight = random.randint(768, min(1440, int(deviceWidth * 0.8)))
-            self.browserConfig["sizes"] = {
-                "height": deviceHeight,
-                "width": deviceWidth,
-            }
-            saveBrowserConfig(self.userDataDir, self.browserConfig)
+            raise TimeoutException
 
-        if self.mobile:
-            screenHeight = deviceHeight + 146
-            screenWidth = deviceWidth
+        searchbar.submit()
+        pointsAfter = self.browser.utils.getAccountPoints()
+
+        searchCount += 1  # Increment the search count regardless of outcome
+        logging.info(f"[BING] {searchCount} searches completed")
+
+        if pointsBefore < pointsAfter:
+            del self.googleTrendsShelf[rootTerm]
         else:
-            screenWidth = deviceWidth + 55
-            screenHeight = deviceHeight + 151
+            logging.debug("Moving passedInTerm to end of list")
+            del self.googleTrendsShelf[rootTerm]
+            self.googleTrendsShelf[rootTerm] = None
 
-        logging.info(f"Screen size: {screenWidth}x{screenHeight}")
-        logging.info(f"Device size: {deviceWidth}x{deviceHeight}")
+        # Ensure delay after each root term search
+        sleep(randint(220, 280))
 
-        if self.mobile:
-            driver.execute_cdp_cmd(
-                "Emulation.setTouchEmulationEnabled",
-                {
-                    "enabled": True,
-                },
-            )
+        if searchRelatedTerms:
+            logging.debug("Starting additional searches for related terms")
+            for i, related_term in enumerate(uniqueTerms):
+                if i >= relatedTermsCount:
+                    break
+                try:
+                    searchbar = self.browser.utils.waitUntilClickable(By.ID, "sb_form_q", timeToWait=40)
+                    searchbar.clear()
+                    logging.debug(f"related term={related_term}")
+                    self.manually_enter_text(searchbar, related_term, min_delay=0.4, max_delay=0.9)  # Use the manual typing function
+                    searchbar.submit()
+                    searchCount += 1
+                    logging.info(f"[BING] {searchCount} searches completed (including additional terms)")
+                    # Ensure delay between each related term search
+                    sleep(randint(220, 280))
+                except TimeoutException:
+                    logging.warning(f"Timeout while searching for related term: {related_term}")
 
-        driver.execute_cdp_cmd(
-            "Emulation.setDeviceMetricsOverride",
-            {
-                "width": deviceWidth,
-                "height": deviceHeight,
-                "deviceScaleFactor": 0,
-                "mobile": self.mobile,
-                "screenWidth": screenWidth,
-                "screenHeight": screenHeight,
-                "positionX": 0,
-                "positionY": 0,
-                "viewport": {
-                    "x": 0,
-                    "y": 0,
-                    "width": deviceWidth,
-                    "height": deviceHeight,
-                    "scale": 1,
-                },
-            },
-        )
-
-        driver.execute_cdp_cmd(
-            "Emulation.setUserAgentOverride",
-            {
-                "userAgent": self.userAgent,
-                "platform": self.userAgentMetadata["platform"],
-                "userAgentMetadata": self.userAgentMetadata,
-            },
-        )
-
-        return driver
-
-    def setupProfiles(self) -> Path:
-        sessionsDir = getProjectRoot() / "sessions"
-        sessionid = f"{self.browserType}"
-        sessionsDir = sessionsDir / sessionid
-        sessionsDir.mkdir(parents=True, exist_ok=True)
-        return sessionsDir
-
-    @staticmethod
-    def getLanguageCountry() -> tuple[str, str]:
-        country = CONFIG.browser.geolocation
-        language = CONFIG.browser.language
-
-        if not language or not country:
-            currentLocale = locale.getlocale()
-            if not language:
-                with contextlib.suppress(ValueError):
-                    language = pycountry.languages.get(
-                        alpha_2=currentLocale[0].split("_")[0]
-                    ).alpha_2
-            if not country:
-                with contextlib.suppress(ValueError):
-                    country = pycountry.countries.get(
-                        alpha_2=currentLocale[0].split("_")[1]
-                    ).alpha_2
-
-        if not language or not country:
-            try:
-                ipapiLocation = ipapi.location()
-                if not language:
-                    language = ipapiLocation["languages"].split(",")[0].split("-")[0]
-                if not country:
-                    country = ipapiLocation["country"]
-            except RateLimited:
-                logging.warning(exc_info=True)
-
-        if not language:
-            language = "en"
-            logging.warning(
-                f"Not able to figure language returning default: {language}"
-            )
-
-        if not country:
-            country = "US"
-            logging.warning(f"Not able to figure country returning default: {country}")
-
-        return language, country
-
-    @staticmethod
-    def getChromeVersion() -> str:
-        chrome_options = ChromeOptions()
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--no-sandbox")
-        driver = WebDriver(options=chrome_options)
-        version = driver.capabilities["browserVersion"]
-
-        driver.close()
-        driver.quit()
-        return version
-
-    def visitURL(self, url: str, duration: int) -> None:
-        self.webdriver.get(url)
-        logging.info(f"Visited {url} for {duration} minutes.")
-        sleep(duration * 60)
+        return searchCount
